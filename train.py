89ccb0b56d805d49ba6a10479db6f302c92a6fb4
Yong <52263269+qbxlvnf11@users.noreply.github.com>
2020-08-18 16:18:59 +0900

    Update train.py

--- train.py
+++ train.py
@@ -14,9 +14,18 @@ from torchsummary import summary
 from datasets.graph_data_reader import DataReader, GraphData
 from models.GCN import GCN
 from models.GAT import GAT
+#from models.GraphWaveletNet import GraphWaveletNet
+#from models.GraphSAGE import GraphSAGE
+from models.GIN import GIN
+from models.GraphUNet import GraphUNet
+from models.GraphResNet import GraphResNet
+from models.GraphDenseNet import GraphDenseNet
+from models.NodeRandomWalkNet import NodeRandomWalkNet
+from models.ExpandedSpatialGraphEmbeddingNet import ExpandedSpatialGraphEmbeddingNet
 from utils import create_directory, save_result_csv
 
-model_list = ['GCN', 'GAT']
+model_list = ['GCN', 'GAT', 'GraphWaveletNet', 'GraphSAGE', 'GIN', 'GraphUNet', 'GraphResNet', 'GraphDenseNet', 'NodeRandomWalkNet', 'ExpandedSpatialGraphEmbeddingNet']
+#model_list = ['GCN', 'GAT', 'GraphWaveletNet', 'GraphSAGE', 'GIN', 'GraphUNet', 'GraphResNet', 'GraphDenseNet', 'NodeRandomWalkNet', 'ExpandedSpatialGraphEmbeddingNet']
 dataset_list = ['IMDB-BINARY', 'IMDB-MULTI', 'PROTEINS', 'ENZYMES', 'NCI1', 'MUTAG']
 readout_list = ['max', 'avg', 'sum']
 
@@ -24,36 +33,47 @@ parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
 
 # Target model & dataset & readout param
 parser.add_argument('--model_list', nargs='+', required=True,
-                    help='target train model list \navailable model: ' + str(model_list)
-                     + '\nable to get multiple values\nALL: choose all available model')
+                    help='target train model list \n'+
+                    'available model: ' + str(model_list)
+                     + '\nable to choose multiple models \n'+
+                     'ALL: choose all available model')
 parser.add_argument('--dataset_list', nargs='+', required=True,
-                    help='target graph classification dataset list \navailable dataset: ' + str(dataset_list)
-                     + '\nable to get multiple values\nALL: choose all available dataset')
+                    help='target graph classification dataset list \n'+
+                    'available dataset: ' + str(dataset_list)
+                     + '\nable to choose multiple datasets \n'+
+                     'ALL: choose all available dataset')
 parser.add_argument('--readout_list', nargs='+', required=True,
-                    help='target readout method list \navailable readout: ' + str(readout_list)
-                     + '\nable to get multiple values\nALL: choose all available readout')
+                    help='target readout method list \n'+
+                    'available readout: ' + str(readout_list)
+                     + '\nable to choose multiple readout methods \n'+
+                     'ALL: choose all available readout')
                      
 # Dataset param
 parser.add_argument('--node_att', default='FALSE',
-                    help='use additional float valued node attributes available in some datasets or not\nTRUE/FALSE')
+                    help='use additional float valued node attributes available in some datasets or not\n'+
+                    'TRUE/FALSE')
 parser.add_argument('--seed', type=int, default=111,
                     help='random seed')
 parser.add_argument('--n_folds', type=int, default=10,
                     help='the number of folds in 10-cross validation')
 parser.add_argument('--threads', type=int, default=0,
-                    help='how many subprocesses to use for data loading \ndefault value 0 means that the data will be loaded in the main process')
+                    help='how many subprocesses to use for data loading \n'+
+                    'default value 0 means that the data will be loaded in the main process')
+
 # Graph data subsampling
 parser.add_argument('--n_graph_subsampling', type=int, default=0,
-                    help='the number of running graph subsampling each train graph data\nrun subsampling 5 times: increasing graph data 5 times')
+                    help='the number of running graph subsampling each train graph data\n'+
+                    'run subsampling 5 times: increasing graph data 5 times')
 parser.add_argument('--graph_node_subsampling', default='TRUE',
-                    help='TRUE: removing node randomly to subsampling and augmentation of graph dataset'
-                    + '\nFALSE: removing edge randomly to subsampling and augmentation of graph dataset')
+                    help='TRUE: removing node randomly to subsampling and augmentation of graph dataset \n'+
+                    'FALSE: removing edge randomly to subsampling and augmentation of graph dataset')
 parser.add_argument('--graph_subsampling_rate', type=float, default=0.2,
                     help='graph subsampling rate')
 
 # Learning param
 parser.add_argument('--cuda', default='TRUE',
-                    help='use cuda device in train process or not\nTRUE/FALSE')
+                    help='use cuda device in train process or not\n'+
+                    'TRUE/FALSE')
 parser.add_argument('--batch_size', type=int, default=32,
                     help='batch size of data')                  
 parser.add_argument('--epochs', type=int, default=50,
@@ -65,7 +85,8 @@ parser.add_argument('--weight_decay', type=float, default=5e-4,
 parser.add_argument('--log_interval', type=int, default=10,
                     help='print log interval in train process')
 parser.add_argument('--save_model', default='TRUE',
-                    help='save model or not\nTRUE/FALSE')
+                    help='save model or not\n'+
+                    'TRUE/FALSE')
 
 # Model param
 parser.add_argument('--n_agg_layer', type=int, default=2,
@@ -74,15 +95,27 @@ parser.add_argument('--agg_hidden', type=int, default=32,
                     help='size of hidden graph aggregation layer')
 parser.add_argument('--fc_hidden', type=int, default=128,
                     help='size of fully-connected layer after readout')
-parser.add_argument('--dropout', type=float, default=0.25,
+parser.add_argument('--dropout', type=float, default=0,
                     help='dropout rate of layer')
-                    
+
+# ExpandedSpatialGraphEmbeddingNet param
+parser.add_argument('--freeze_layer', default='FALSE',
+                    help='the number of graph aggregation layers')
+parser.add_argument('--fc_layer_type', default='A',
+                    help='fully-connected layer type of ExpandedSpatialGraphEmbeddingNet')
+parser.add_argument('--concat_dropout', type=float, default=0.0,
+                    help='dropout rate of concat layer of ExpandedSpatialGraphEmbeddingNet')
+
 args = parser.parse_args()
 
 args.cuda = (args.cuda.upper()=='TRUE')
 args.save_model = (args.save_model.upper()=='TRUE')
 args.node_att = (args.node_att.upper()=='TRUE')
 args.graph_node_subsampling = (args.graph_node_subsampling.upper()=='TRUE')
+args.freeze_layer = (args.freeze_layer.upper()=='TRUE')
+
+# Build random walk or not (if mode == NodeRandomWalkNet or ExpandedSpatialGraphEmbeddingNet)
+random_walk = ('NodeRandomWalkNet' in args.model_list or 'ExpandedSpatialGraphEmbeddingNet' in args.model_list or 'ALL' in args.model_list)
 
 # Choose target graph classification model
 if 'ALL' in args.model_list:
@@ -125,6 +158,11 @@ else:
   
 print('Using device in train process:', device)
 
+if args.n_graph_subsampling > 0 and args.graph_node_subsampling:
+  print('graph subsampling: random node removal')
+elif args.n_graph_subsampling > 0 and not args.graph_node_subsampling:
+  print('graph subsampling: random edge removal')
+
 for dataset_name in args.dataset_list:
     print('-'*50)
     
@@ -132,8 +170,9 @@ for dataset_name in args.dataset_list:
     # Build graph data reader: IMDB-BINARY, IMDB-MULTI, ...
     datareader = DataReader(data_dir='./datasets/%s/' % dataset_name.upper(),
                         rnd_state=np.random.RandomState(args.seed),
-                        folds=args.n_folds,                    
-                        use_cont_node_attr=False)
+                        folds=args.n_folds,           
+                        use_cont_node_attr=False,
+                        random_walk=random_walk)
     
     for model_name in args.model_list:
       for i, readout_name in enumerate(args.readout_list):
@@ -158,12 +197,89 @@ for dataset_name in args.dataset_list:
                     dropout=args.dropout,
                     readout=readout_name,
                     device=device).to(device)
-
+#        elif model_name == 'GraphWaveletNet':
+#            model = GraphWaveletNet(n_feat=datareader.data['features_dim'],
+#                    n_class=datareader.data['n_classes'],
+#                    n_layer=args.n_agg_layer,
+#                    agg_hidden=args.agg_hidden,
+#                    fc_hidden=args.fc_hidden,
+#                    dropout=args.dropout,
+#                    readout=readout_name,
+#                    device=device,
+#                    n_node=datareader.data['N_nodes_max']).to(device)
+        elif model_name == 'GraphSAGE':
+            model = GraphUNet(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)
+        elif model_name == 'GIN':
+            model = GIN(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)
+        elif model_name == 'GraphUNet':
+            model = GraphUNet(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)
+        elif model_name == 'GraphResNet':
+            model = GraphResNet(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)
+        elif model_name == 'GraphDenseNet':
+            model = GraphDenseNet(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)
+        elif model_name == 'NodeRandomWalkNet':
+            model = NodeRandomWalkNet(n_feat=datareader.data['features_dim'],
+                    n_class=datareader.data['n_classes'],
+                    n_layer=args.n_agg_layer,
+                    agg_hidden=args.agg_hidden,
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device).to(device)        
+        elif model_name == 'ExpandedSpatialGraphEmbeddingNet':
+            model = ExpandedSpatialGraphEmbeddingNet(
+                    n_class=datareader.data['n_classes'],
+                    fc_hidden=args.fc_hidden,
+                    dropout=args.dropout,
+                    readout=readout_name,
+                    device=device,
+                    dataset_name=dataset_name,
+                    n_folds=args.n_folds,
+                    freeze_layer=args.freeze_layer,
+                    fc_layer_type=args.fc_layer_type,
+                    concat_dropout=args.concat_dropout).to(device)  
+                                                         
         print(model)
         print('Readout:', readout_name)
         
         # Train & test each fold
         acc_folds = []
+        time_folds = []
         for fold_id in range(args.n_folds):
             print('\nFOLD', fold_id)
             loaders = []
@@ -173,6 +289,7 @@ for dataset_name in args.dataset_list:
                     gdata = GraphData(fold_id=fold_id,
                                        datareader=datareader,
                                        split=split,
+                                       random_walk=random_walk,
                                        n_graph_subsampling=args.n_graph_subsampling,
                                        graph_node_subsampling=args.graph_node_subsampling,
                                        graph_subsampling_rate=args.graph_subsampling_rate)
@@ -180,6 +297,7 @@ for dataset_name in args.dataset_list:
                     gdata = GraphData(fold_id=fold_id,
                                        datareader=datareader,
                                        split=split,
+                                       random_walk=random_walk,
                                        n_graph_subsampling=0,
                                        graph_node_subsampling=args.graph_node_subsampling,
                                        graph_subsampling_rate=args.graph_subsampling_rate)      
@@ -209,6 +327,7 @@ for dataset_name in args.dataset_list:
             
             # Train function
             def train(train_loader):
+                total_time_iter = 0
                 model.train()
                 start = time.time()
                 train_loss, n_samples = 0, 0
@@ -216,11 +335,15 @@ for dataset_name in args.dataset_list:
                     for i in range(len(data)):
                         data[i] = data[i].to(device)
                     optimizer.zero_grad()
-                    output = model(data)
+                    if model_name == 'ExpandedSpatialGraphEmbeddingNet':
+                        output = model(data, fold_id, 'train')
+                    else:
+                        output = model(data)
                     loss = loss_fn(output, data[4])
                     loss.backward()
                     optimizer.step()
                     time_iter = time.time() - start
+                    total_time_iter += time_iter
                     train_loss += loss.item() * len(output)
                     n_samples += len(output)
                     if batch_idx % args.log_interval == 0 or batch_idx == len(train_loader) - 1:
@@ -228,6 +351,7 @@ for dataset_name in args.dataset_list:
                             epoch, n_samples, len(train_loader.dataset),
                             100. * (batch_idx + 1) / len(train_loader), loss.item(), train_loss / n_samples, time_iter / (batch_idx + 1) ))
                 scheduler.step()
+                return total_time_iter / (batch_idx + 1)
             
             # Test function
             def test(test_loader):
@@ -238,7 +362,10 @@ for dataset_name in args.dataset_list:
                 for batch_idx, data in enumerate(test_loader):
                     for i in range(len(data)):
                         data[i] = data[i].to(device)
-                    output = model(data)
+                    if model_name == 'ExpandedSpatialGraphEmbeddingNet':
+                        output = model(data, fold_id, 'test')
+                    else:
+                        output = model(data)
                     loss = loss_fn(output, data[4], reduction='sum')
                     test_loss += loss.item()
                     n_samples += len(output)
@@ -256,21 +383,28 @@ for dataset_name in args.dataset_list:
                                                                                                       correct, 
                                                                                                       n_samples, acc))
                 return acc
-    
+            
+            # Loss function
             loss_fn = F.cross_entropy
+            
+            total_time = 0
             for epoch in range(args.epochs):
-                train(loaders[0])
+                total_time_iter = train(loaders[0])
+                total_time += total_time_iter
                 acc = test(loaders[1])
+            acc_folds.append(round(acc,2))
+            time_folds.append(round(total_time/args.epochs,2))
+            
             # Save model
             if args.save_model:
                 print('Save model ...')
                 create_directory('./save_model')
+                create_directory('./save_model/' + model_name)
                 
-                file_name = model_name + '_' + dataset_name + '_' + readout_name + '_' + str(fold_id) + '.pt'
+                file_name = model_name + '_' + dataset_name + '_' + readout_name + '_' + str(fold_id) + '_' + str(args.n_agg_layer) + '_h' + str(args.agg_hidden) + '.pt'
           
-                torch.save(model, './save_model/' + file_name)
+                torch.save(model, './save_model/' + model_name + '/' + file_name)
                 print('Complete to save model')
-            acc_folds.append(round(acc,2))
     
         print(acc_folds)
         print('{}-fold cross validation avg acc (+- std): {} ({})'.format(args.n_folds, statistics.mean(acc_folds), statistics.stdev(acc_folds)))
@@ -286,12 +420,13 @@ for dataset_name in args.dataset_list:
           result_list.append(str(acc_fold))
         result_list.append(statistics.mean(acc_folds))
         result_list.append(statistics.stdev(acc_folds))
+        result_list.append(statistics.mean(time_folds))
         
-        file_name = model_name + '_' + str(args.n_agg_layer) + '_' + '10_cross_validation.csv'
+        file_name = model_name + '_' + str(args.n_agg_layer) + '_h' + str(args.agg_hidden) + '_' + '10_cross_validation.csv'
         if args.n_graph_subsampling > 0 and args.graph_node_subsampling:
-          file_name = model_name + '_' + str(args.n_agg_layer) + '_' + 'node_graph_subsampling' + '_' + '10_cross_validation.csv'
+          file_name = model_name + '_' + str(args.n_agg_layer) + '_h' + str(args.agg_hidden) + '_' + 'node_graph_subsampling' + '_' + '10_cross_validation.csv'
         elif args.n_graph_subsampling > 0:
-          file_name = model_name + '_' + str(args.n_agg_layer) + '_' + 'edge_graph_subsampling' + '_' + '10_cross_validation.csv'
+          file_name = model_name + '_' + str(args.n_agg_layer) + '_h' + str(args.agg_hidden) + '_' + 'edge_graph_subsampling' + '_' + '10_cross_validation.csv'
         
         if i == 0:
           save_result_csv('./test_result/' + model_name + '/' + file_name, result_list, True)
